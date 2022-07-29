a4e3dc2aef4ff5e8b02c10b2eb0d8a9591662277
Alexis Castillo <alexis.castillo.pimienta@gmail.com>
2022-07-28 19:58:21 -0600

    inicio npx

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AppRouter from './router/AppRouter';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "./styles/estilos.css";
+
+function App() {
+  return <AppRouter />
+}
+
+export default App;
