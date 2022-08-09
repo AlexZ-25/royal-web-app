618f8d01eee3183d2b484c2ccf161e662b0be0c8
castaltmc <CASTAL@chrobinson.com>
2022-08-08 20:46:11 -0500

    ran build

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "./styles/style.css";
 
-function App() {
+const App = () => {
   return <AppRouter />
 }
 
